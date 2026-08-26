220b6e59ffa43d6e7f7e297360a0a4727d1baceb
Gaurav Rathee <109849811+aurav666@users.noreply.github.com>
2026-08-27 01:33:38 +0530

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+// Simple service worker to cache the game for offline play (feature 9)
+const CACHE_NAME = 'monkeypath-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/sw.js'
+  // add icon paths if present: '/icons/icon-192.png', '/icons/icon-512.png'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); } )))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then(res => res || fetch(evt.request))
+  );
+});
